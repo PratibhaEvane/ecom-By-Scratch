ea86fd4279c13596e1fc4eed9d266c1043f21cea
Pratibha <misspratibhaa@gmail.com>
2023-01-21 15:11:00 +0530

    Change Navbar Menu after seller Login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +6,27 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
-
+export class HeaderComponent implements OnInit {
+  menuUrlType: string = ''
   constructor(private route: Router) { }
+  // 
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      console.log(val.url)
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.warn('in seller area')
+          this.menuUrlType = 'seller'
+        }
+        else {
+          console.log('outside the seller area')
+          this.menuUrlType = 'default'
+
+        }
+      }
+    })
+  }
+
+
 
 }
