a1c0ef0d0393082c0a4dcaf7a2dd38ec64b4e456
Pratibha <misspratibhaa@gmail.com>
2023-01-28 23:38:43 +0530

    dynamic search with API

--- src/app/home-page-service/search-bar.service.ts
+++ src/app/home-page-service/search-bar.service.ts
@@ -8,7 +8,7 @@ import { Product } from '../data-type';
 export class SearchBarService {
 
   constructor(private http: HttpClient) { }
-  //function for product search on navbar via api
+  // below function is created to fecth the deatils of searched product on navbar from api
   product_search(query_product: string) {
     return this.http.get<Product[]>(`http://localhost:3000/added_product?q=${query_product}`)
   }
